a965e5a3e72495d79c868a003187648225c2fdf3
cbrown <vanguard737@gmail.com>
2024-11-02 06:32:28 -0500

    black/isort/ruff

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,20 +1,26 @@
-import os, re, shutil, time, json
 import argparse
+import json
+import math
+import os
 import pickle
+import random
+import re
+import shutil
+import time
 import warnings
 import zipfile
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import math, random
-import matplotlib.pyplot as plt
 
-from .pyrosetta_utils import *
-from .colabdesign_utils import *
 from .biopython_utils import *
+from .colabdesign_utils import *
 from .generic_utils import *
+from .pyrosetta_utils import *
 
 # suppress warnings
-#os.environ["SLURM_STEP_NODELIST"] = os.environ["SLURM_NODELIST"]
-warnings.simplefilter(action='ignore', category=FutureWarning)
-warnings.simplefilter(action='ignore', category=DeprecationWarning)
-warnings.simplefilter(action='ignore', category=BiopythonWarning)
+# os.environ["SLURM_STEP_NODELIST"] = os.environ["SLURM_NODELIST"]
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.simplefilter(action="ignore", category=DeprecationWarning)
+warnings.simplefilter(action="ignore", category=BiopythonWarning)
