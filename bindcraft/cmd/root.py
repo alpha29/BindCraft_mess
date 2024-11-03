65f414972a828b71bf71a8ac6d0cd23b8a729d11
cbrown <vanguard737@gmail.com>
2024-11-03 14:44:07 -0600

    CLI

--- bindcraft/cmd/root.py
+++ bindcraft/cmd/root.py
@@ -2,14 +2,29 @@ import os
 import typer
 
 from bindcraft import __version__
+from bindcraft.runner import main, set_up_pyrosetta
 
 app = typer.Typer()
 
 BINDCRAFT_HOME = os.getenv("BINDCRAFT_HOME", os.getcwd())
 
 @app.command()
+def setup_pyrosetta():
+    """
+    Set up pyrosetta.
+    """
+    typer.echo("Setting up pyrosetta...")
+    set_up_pyrosetta()
+    typer.echo("Done.")
+
+
+@app.command()
 def run():
+    """
+    Run bindcraft-based binder design.
+    """
     typer.echo("Running...")
+    main()
     typer.echo("Done.")
 
 @app.command()
